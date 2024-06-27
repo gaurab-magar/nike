2309773b4567537476385d25ffd2aa27cc84b8f1
Your Name <gdikshan10@gmail.com>
2024-06-27 12:09:30 +0545

    swiper added to the hero section for nice effect and show the product

---
+++ src/app/Components/HeroBanner.js
@@ -0,0 +1,39 @@
+'use client'
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Autoplay } from 'swiper/modules';
+
+const HeroBanner = () => {
+  return (
+    <section>
+        <Swiper
+            spaceBetween={30}
+            centeredSlides={true}
+            autoplay={{delay:2000,
+            disableOnInteraction:false}}
+            modules={[Autoplay]}
+            className="mySwiper h-[15rem] md:h-[28rem]"
+
+            >
+            <SwiperSlide>
+                <img src="/assets/slide-3.png" alt="hero-banner-1"
+                className="w-full h-full object-cover relative" />
+                <button className='absolute bottom-3 left-3  px-[15px] md:px-[40px] py-[10px] md:py-[25px] bg-black/[0.7] text-white font-bold text-sm md:text-lg uppercase cursor-pointer hover:opacity-90'>Shop now</button>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src="/assets/slide-2.png" alt="hero-banner-2"
+                className="w-full h-full object-cover relative" />
+                <button className='absolute bottom-3 left-3  px-[15px] md:px-[40px] py-[10px] md:py-[25px] bg-black/[0.7] text-white font-bold text-sm md:text-lg uppercase cursor-pointer hover:opacity-90'>Shop now</button>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src="/assets/slide-1.png" alt="hero-banner-2"
+                className="w-full h-full object-cover relative" />
+                <button className='absolute bottom-3 left-3  px-[15px] md:px-[40px] py-[10px] md:py-[25px] bg-black/[0.7] text-white font-bold text-sm md:text-lg uppercase cursor-pointer hover:opacity-90'>Shop Now</button>
+            </SwiperSlide>         
+        </Swiper>
+    </section>
+  )
+}
+
+export default HeroBanner
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import HeroBanner from "./Components/HeroBanner";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-4">
-     <h3>I AM GAURAB THAPA MAGAR AND HAVE OVER THREE YEARS OF EXPERRIENCE IN FRONTEND DEVELOPMENT</h3>
+    <main className="flex min-h-screen flex-col p-1">
+      <HeroBanner />
     </main>
   );
 }
